8b80adbfa0aa4934e85d0b37d5b9ee5665e6214c
Luiz Lima <luizoscar.lima@gmail.com>
2017-10-19 23:33:33 -0200

    Add domain project

---
+++ ProjetoH2O.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoH2O.Domain
+{
+    public class Class1
+    {
+    }
+}
